f7b781634c6a6afb162c23153312cf8ce426c763
tk-prateek-singh <pratik.24b1541137@abes.ac.in>
2026-07-29 23:38:52 +0530

    Time: 2 ms (92.13%) | Memory: 47.8 MB (38.54%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int left=0;
+       int right=0;
+       while(right<nums.length){
+        if(nums[right]!=0){
+            int temp=nums[right];
+            nums[right]=nums[left];
+            nums[left]=temp;
+            right++;
+            left++;
+        }
+        else{
+            right++;
+        }
+       }
+    }
+}
