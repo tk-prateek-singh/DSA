6018b12486cff60442c5d6c33db100841f24d1ad
tk-prateek-singh <pratik.24b1541137@abes.ac.in>
2026-08-20 22:20:34 +0530

    Time: 5 ms (82.09%) | Memory: 66.9 MB (48.62%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSum=0;
+        int minSum=0;
+        int currMax=0;
+        int currMin=0;
+        for(int i=0;i<nums.length;i++){
+            currMax+=nums[i];
+            currMin+=nums[i];
+            maxSum=Math.max(maxSum,currMax);
+            minSum=Math.min(minSum,currMin);
+            if(currMax<0){
+                currMax=0;
+            }
+            if(currMin>0){
+                currMin=0;
+            }
+        }
+        return Math.max(maxSum,Math.abs(minSum));
+    }
+}
